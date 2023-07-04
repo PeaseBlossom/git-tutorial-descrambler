8a7f29392541d26f88fc54182a9c3f61d400362e
worku005 <dirk-jan.vanworkum@wur.nl>
2023-07-04 15:01:36 +0200

    add test for letter 6

--- tests/test_descrambler.py
+++ tests/test_descrambler.py
@@ -13,6 +13,12 @@ class TestDescrambler(unittest.TestCase):
         output_lines = descrambler1.descramble(input_lines)
         self.assertEqual(expected_lines, output_lines)
 
+    def test_descrambler6(self):
+        input_lines = ['Daarest Blue-ba-de-dae,\n']
+        expected_lines = ['Dearest Blue-da-ba-dee,\n']
+        output_lines = descrambler1.descramble(input_lines)
+        self.assertEqual(expected_lines, output_lines)
+
 
 if __name__ == '__main__':
     unittest.main()
